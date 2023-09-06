a7405cdb7eac36c28a35fc2df23f875584dcda2d
hye-yeong lee <i-e@yukoyuko.co.jp>
2023-09-06 19:21:51 +0900

    chor: set apollo provider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 import React, { useState } from 'react';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 
 import Roles from './components/roles'
 import Teams from './components/teams'
 import People from './components/people'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache() //graphQl이 한 번 받아 온 내용을 불필요하게 받아오지 않게끔 캐싱을 하는 기능 
+})
 
 function App() {
 
@@ -21,24 +27,28 @@ function App() {
     ].map((_menu, key) => {
       return (
         <li key={key} className={menu === _menu ? 'on' : ''}
-          onClick={() => { setMenu(_menu); }}>{_menu}</li>
+          onClick={() => { setMenu(_menu); }}>
+            {_menu}
+        </li>
       );
     });
   }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Company Management</h1>
-        <nav>
-          <ul>
-            {NavMenus()}
-          </ul>
-        </nav>
-      </header>
-      <main>
-        {mainComp[menu]}
-      </main>
+      <ApolloProvider client={client}>
+        <header className="App-header">
+          <h1>Company Management</h1>
+          <nav>
+            <ul>
+              {NavMenus()}
+            </ul>
+          </nav>
+        </header>
+        <main>
+          {mainComp[menu]}
+        </main>
+      </ApolloProvider>
     </div>
   );
 }
